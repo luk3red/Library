b6a4dee465a420d03d6f09d5c97e4f184a8ff4e7
luk3red <78453588+luk3red@users.noreply.github.com>
2022-09-16 17:26:27 +0200

    Delete workspace/src directory

--- workspace/src/DAO/DAOAMMint.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.sql.Connection;
-
-public interface DAOAMMint {
-
-	boolean LogInAmm(Connection connection, String username, String password) throws Exception;
-
-}
--- workspace/src/DAO/DAOaccessiInteface.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.sql.Connection;
-
-public interface DAOaccessiInteface {
-
-	String Permesso(Connection connection, String ID_utente, String ID_owner, String ID_libreria) throws Exception;
-
-}
