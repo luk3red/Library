f72d6647dea0845c2d67d3c644292a761945fabe
luk3red <78453588+luk3red@users.noreply.github.com>
2022-09-16 17:28:56 +0200

    Delete src directory

--- src/DAO/DAOanagraficaint.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.sql.Connection;
-
-public interface DAOanagraficaint {
-
-	String ModAna(Connection connection, String nome, String cognome, String email, String username) throws Exception;
-
-}
